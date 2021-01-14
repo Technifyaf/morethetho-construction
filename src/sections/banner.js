61055793f8f7049226fcd8ac9b9964192709f8ff
Technifyaf <Technifyaf@gmail.com>
2021-01-14 22:10:50 +0200

    favicon

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -6,148 +6,149 @@ import Select from 'components/select';
 import bannerBg from 'assets/images/morethetho_banner.jpg';
 import mapMarker from 'assets/images/icons/map-marker.png';
 
-
 const options = [
-  {
-    id: 1,
-    label: 'Brooklyn, New york',
-    value: 'Brooklyn, New york',
-  },
-  {
-    id: 2,
-    label: 'Atlanta, Georgia',
-    value: 'Atlanta, Georgia',
-  },
-  {
-    id: 3,
-    label: 'Minneapolis, Minnesota',
-    value: 'Minneapolis, Minnesota',
-  },
-  {
-    id: 4,
-    label: 'Chicago, Illinois',
-    value: 'Chicago, Illinois',
-  },
+	{
+		id: 1,
+		label: 'Brooklyn, New york',
+		value: 'Brooklyn, New york',
+	},
+	{
+		id: 2,
+		label: 'Atlanta, Georgia',
+		value: 'Atlanta, Georgia',
+	},
+	{
+		id: 3,
+		label: 'Minneapolis, Minnesota',
+		value: 'Minneapolis, Minnesota',
+	},
+	{
+		id: 4,
+		label: 'Chicago, Illinois',
+		value: 'Chicago, Illinois',
+	},
 ];
 
 export default function Banner() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('submitting...');
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submitting...');
+	};
 
-  return (
-    <Box as="section" id="home" sx={styles.section}>
-      <Container>
-        <Box sx={styles.contentWrapper}>
-          <Box sx={styles.bannerContent}>
-            <Heading as="h" sx={styles.heroTitle}>
-            Over 5 years of borehole drilling excellence
-            </Heading>
-            <Text as="p" sx={styles.desc}>
-            We have installed more than 30+ wells and 50 boreholes in Maseru, Mohale'shoek, Maputsoe, Thaba Tseka, Quthing, Mokholtong and Leribe. 
-            </Text>
-         
-              <a
-						href='https://forms.gle/a4zRnRWchcUZaEG88'
-						ml={2}
-						sx={styles.button}
-						variant='buttons.primary'
-					>
-					 <Button sx={styles.button}>Request quotation</Button>
-					</a>
-      
-          </Box>
-        </Box>
-      </Container>
-    </Box>
-  );
+	return (
+		<Box as='section' id='home' sx={styles.section}>
+			<Container>
+				<Box sx={styles.contentWrapper}>
+					<Box sx={styles.bannerContent}>
+						<Heading as='h' sx={styles.heroTitle}>
+							Your preferred drilling partner over the past 8 years
+						</Heading>
+						<Text as='p' sx={styles.desc}>
+							Morethetho Construction has been engaged in many projects
+							throughout Lesotho for the past 8 years. We have had an
+							opportunity to drill on different geological conditions for
+							boreholes and exploration of underground exploration.
+						</Text>
+
+						<a
+							href='https://forms.gle/a4zRnRWchcUZaEG88'
+							ml={2}
+							sx={styles.button}
+							variant='buttons.primary'
+						>
+							<Button sx={styles.button}>Request quotation</Button>
+						</a>
+					</Box>
+				</Box>
+			</Container>
+		</Box>
+	);
 }
 
 const styles = {
-  section: {
-    background: `url(${bannerBg}) no-repeat center top / cover`,
-    backgroundSize: ['100%', null, null, null, 'cover'],
-  },
-  contentWrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    minHeight: [null, null, null, null, '50vh', '100vh'],
-  },
-  bannerContent: {
-    backgroundColor: rgba('#404040', 1),
-    boxShadow: [
-      '0px 10px 16px rgba(52, 61, 72, 0.12)',
-      null,
-      null,
-      null,
-      'none',
-    ],
-    maxWidth: [null, null, null, 600, 500, null, 650],
-    padding: [
-      '20px',
-      '30px',
-      null,
-      null,
-      null,
-      '30px 50px 60px',
-      '50px 60px 90px',
-    ],
-    borderRadius: 5,
-    m: ['110px 0 0', null, null, '110px auto 0', '60px 0 0', null, 0],
-    '@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
-      maxWidth: 515,
-      mt: 70,
-      padding: '30px 50px 65px',
-    },
-  },
-  heroTitle: {
-    fontSize: [22, 28, 28, 40, , 5, 6],
-    color : '#fff',
-    fontWeight: 700,
-    letterSpacing: 'heading',
-    lineHeight: [1.4, null, null, null, null, null, 1.57],
-    '@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
-      fontSize: 40,
-    },
-  },
-  desc: {
-    color : '#fff',
-    fontSize: [15, 16, 15, 17],
-    lineHeight: [1.53, 1.53, 1.53, 2, 2.4, 2, 2.48],
-    maxWidth: 440,
-    marginTop: [15, 15, 15, null, null, null, 30],
-    '@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
-      mt: 15,
-    },
-  },
-  input: {
-    backgroundColor: rgba('#fff', 0.08),
-    borderRadius: '5px',
-    borderColor: '#020718',
-    color: rgba('#fff', 0.8),
-    flexGrow: 1,
-    fontFamily: 'body',
-    height: 'auto',
-    px: '30px',
-    py: '0',
-    minHeight: [50, 50, 60],
-    width: ['100%', 'auto'],
-    '::placeholder': {
-      color: rgba('#fff', 0.8),
-    },
-  },
-  
-  button: {
-    fontSize: 20,
-    textDecoration : 'none',
-    fontWeight: 700,
-    marginTop: 20,
-    width: '100%',
-    minHeight: [50, null, null, null, 60],
-    fontSize: [16, 16, 16, 20],
-    ':focus': {
-      outline: '0 none',
-    },
-  },
+	section: {
+		background: `url(${bannerBg}) no-repeat center top / cover`,
+		backgroundSize: ['100%', null, null, null, 'cover'],
+	},
+	contentWrapper: {
+		display: 'flex',
+		alignItems: 'center',
+		minHeight: [null, null, null, null, '50vh', '100vh'],
+	},
+	bannerContent: {
+		backgroundColor: rgba('#404040', 0.5),
+		boxShadow: [
+			'0px 10px 16px rgba(52, 61, 72, 0.12)',
+			null,
+			null,
+			null,
+			'none',
+		],
+		maxWidth: [null, null, null, 600, 500, null, 800],
+		padding: [
+			'20px',
+			'30px',
+			null,
+			null,
+			null,
+			'30px 50px 60px',
+			'50px 60px 90px',
+		],
+		borderRadius: 5,
+		m: ['110px 0 0', null, null, '110px auto 0', '60px 0 0', null, 0],
+		'@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
+			maxWidth: 515,
+			mt: 70,
+			padding: '30px 50px 65px',
+		},
+	},
+	heroTitle: {
+		fontSize: [22, 28, 28, 40, , 5, 6],
+		color: '#fff',
+		fontWeight: 700,
+		letterSpacing: 'heading',
+		lineHeight: [1.4, null, null, null, null, null, 1.57],
+		'@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
+			fontSize: 40,
+		},
+	},
+	desc: {
+		color: '#fff',
+		fontSize: [15, 16, 15, 17],
+		lineHeight: [1.53, 1.53, 1.53, 2, 2.4, 2, 2.48],
+		maxWidth: 440,
+		marginTop: [15, 15, 15, null, null, null, 30],
+		'@media only screen and (min-height: 720px) and (max-height: 760px), (min-width: 1501px) and (max-width: 1560px) ': {
+			mt: 15,
+		},
+	},
+	input: {
+		backgroundColor: rgba('#fff', 0.08),
+		borderRadius: '5px',
+		borderColor: '#020718',
+		color: rgba('#fff', 0.8),
+		flexGrow: 1,
+		fontFamily: 'body',
+		height: 'auto',
+		px: '30px',
+		py: '0',
+		minHeight: [50, 50, 60],
+		width: ['100%', 'auto'],
+		'::placeholder': {
+			color: rgba('#fff', 0.8),
+		},
+	},
+
+	button: {
+		fontSize: 20,
+		textDecoration: 'none',
+		fontWeight: 700,
+		marginTop: 20,
+		width: '100%',
+		minHeight: [50, null, null, null, 60],
+		fontSize: [16, 16, 16, 20],
+		':focus': {
+			outline: '0 none',
+		},
+	},
 };
--- src/sections/featured-space.js
+++ src/sections/featured-space.js
@@ -10,13 +10,11 @@ import drilling from 'assets/images/gallery/morethetho_gallery_1.jpg';
 import survey from 'assets/images/gallery/morethetho_gallery_6.jpg';
 import casing from 'assets/images/gallery/casing_2.jpg';
 import flashing from 'assets/images/gallery/flushing.jpg';
-import testing from 'assets/images/gallery/casing.jpg'
+import testing from 'assets/images/gallery/casing.jpg';
 import water from 'assets/images/gallery/water_pump.jpg';
 import slider1 from 'assets/images/features/1.jpg';
 import slider2 from 'assets/images/features/2.jpg';
 import slider3 from 'assets/images/features/3.jpg';
-
-
 
 import SwiperCore, { Autoplay, Pagination, EffectFade } from 'swiper';
 // Import Swiper React components
@@ -25,261 +23,253 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 SwiperCore.use([Autoplay, Pagination, EffectFade]);
 
 const data = {
-  feature: [
-    {
-      id: 1,
-      icon: survey,
-      title: 'Surveying',
-      description: 'Drilling and flashing.',
-      path : '/services/surveying'
-    },
-    {
-      id: 1,
-      icon: drilling,
-      title: 'Drilling',
-      description: 'Drilling and flashing.',
-      path : '/services/drilling'
-
-    },
-    {
-      id: 2,
-      icon: flashing,
-      title: 'Flushing',
-      description: 'Surveying, casing, and yield casing',
-      path : '/services/flushing'
-
-    },
-    {
-      id: 3,
-      icon: casing,
-      title: ' Casing',
-      description: 'Installation and maintenance',
-      path : '/services/casing'
-
-    },
-    {
-      id: 4,
-      icon: testing,
-      title: 'Yield testing',
-      description: 'Installation and maintenance',
-      path : '/services/yielding'
-
-    },
-    {
-      id: 5,
-      icon: water,
-      title: 'Water pump',
-      description: 'Installation and maintenance',
-      path : '/services/pump'
-
-    },
-  ],
-  gallery: [
-    {
-      id: 1,
-      image: slider1,
-      title: 'Drilling ',
-      desc: 'Borehole and blast drilling',
-    },
-    {
-      id: 2,
-      image: slider2,
-      title: 'Surveying and casing',
-      desc: 'Borehole and yield casing',
-    },
-    {
-      id: 3,
-      image: slider3,
-      title: 'Water pump ',
-      desc: 'Installation and maintenance',
-    },
-  ],
+	feature: [
+		{
+			id: 1,
+			icon: survey,
+			title: 'Surveying',
+			description: 'Drilling and flashing.',
+			path: '/services/surveying',
+		},
+		{
+			id: 1,
+			icon: drilling,
+			title: 'Drilling',
+			description: 'Drilling and flashing.',
+			path: '/services/drilling',
+		},
+		{
+			id: 2,
+			icon: flashing,
+			title: 'Flushing',
+			description: 'Surveying, casing, and yield casing',
+			path: '/services/flushing',
+		},
+		{
+			id: 3,
+			icon: casing,
+			title: ' Casing',
+			description: 'Installation and maintenance',
+			path: '/services/casing',
+		},
+		{
+			id: 4,
+			icon: testing,
+			title: 'Yield testing',
+			description: 'Installation and maintenance',
+			path: '/services/yielding',
+		},
+		{
+			id: 5,
+			icon: water,
+			title: 'Water pump',
+			description: 'Installation and maintenance',
+			path: '/services/pump',
+		},
+	],
+	gallery: [
+		{
+			id: 1,
+			image: slider1,
+			title: 'Drilling ',
+			desc: 'Borehole and blast drilling',
+		},
+		{
+			id: 2,
+			image: slider2,
+			title: 'Surveying and casing',
+			desc: 'Borehole and yield casing',
+		},
+		{
+			id: 3,
+			image: slider3,
+			title: 'Water pump ',
+			desc: 'Installation and maintenance',
+		},
+	],
 };
 
 const FeaturedSpace = () => {
-  const isPause = useRef(false);
-  const swiperRef = useRef(null);
-  const [togglePlay, setTogglePlay] = useState(false);
-  const [currentWidth, setCurrentWidth] = useState(0);
+	const isPause = useRef(false);
+	const swiperRef = useRef(null);
+	const [togglePlay, setTogglePlay] = useState(false);
+	const [currentWidth, setCurrentWidth] = useState(0);
 
-  let time = 3;
-  let tick, percentTime;
+	let time = 3;
+	let tick, percentTime;
 
-  function startProgressbar() {
-    resetProgressbar();
-    percentTime = 0;
-    tick = setInterval(interval, 10);
-  }
+	function startProgressbar() {
+		resetProgressbar();
+		percentTime = 0;
+		tick = setInterval(interval, 10);
+	}
 
-  function interval() {
-    if (isPause.current === false) {
-      percentTime += 1 / (time + 0.1);
-      setCurrentWidth(percentTime);
-      if (percentTime >= 100) {
-        swiperRef?.current?.swiper?.slideNext();
-        startProgressbar();
-      }
-    }
-  }
+	function interval() {
+		if (isPause.current === false) {
+			percentTime += 1 / (time + 0.1);
+			setCurrentWidth(percentTime);
+			if (percentTime >= 100) {
+				swiperRef?.current?.swiper?.slideNext();
+				startProgressbar();
+			}
+		}
+	}
 
-  function resetProgressbar() {
-    setCurrentWidth(0);
-    clearTimeout(tick);
-  }
+	function resetProgressbar() {
+		setCurrentWidth(0);
+		clearTimeout(tick);
+	}
 
-  useEffect(() => {
-    startProgressbar();
-  }, []);
+	useEffect(() => {
+		startProgressbar();
+	}, []);
 
-  const handleToggle = () => {
-    isPause.current = !isPause.current;
-    setTogglePlay((prev) => !prev);
-  };
+	const handleToggle = () => {
+		isPause.current = !isPause.current;
+		setTogglePlay((prev) => !prev);
+	};
 
-  return (
-    <Box id="feature" as="section" sx={styles.section}>
-      <Container>
-        <Box sx={styles.contentWrapper}>
-          <Box >
-            <SectionHeading
-              sx={styles.heading}
-              title="Our services"
-              description="Our primary services are drilling services we have state of the art drilling equipment, thouroughly trained personnel and expert geologists."
-            />
-            <Box sx={styles.featureWrapper}>
-              {data?.feature?.map((feature) => (
-                <Feature key={feature.id} data={feature} />
-              ))}
-           
-            </Box>
-          </Box>
-         
-        </Box>
-      </Container>
-    </Box>
-  );
+	return (
+		<Box id='feature' as='section' sx={styles.section}>
+			<Container>
+				<Box sx={styles.contentWrapper}>
+					<Box>
+						<SectionHeading
+							sx={styles.heading}
+							title='Our services'
+							description='Our primary services are drilling services we have state of the art drilling equipment, thouroughly trained personnel and expert geologists.'
+						/>
+						<Box sx={styles.featureWrapper}>
+							{data?.feature?.map((feature) => (
+								<Feature key={feature.id} data={feature} />
+							))}
+						</Box>
+					</Box>
+				</Box>
+			</Container>
+		</Box>
+	);
 };
 
 export default FeaturedSpace;
 
 const styles = {
-  section: {
-    pt: [30, null, null, null, 60],
-    pb: [30, null, null, 50, 60],
-  },
-  contentWrapper: {
-    gap: [null, null, null, null, '30px'],
-    display: ['flex', null, null, null, 'grid'],
-    alignItems: 'center',
-    flexDirection: ['column', null, null, null, null],
-    gridTemplateColumns: ['unset', null, null, null, 'repeat1,fr)'],
-  },
-  leftContent: {
-    m: [0, '30px 0px 0', '30px 50px 0', 0],
-  },
-  heading: {
-    textAlign: ['center', null, null, null, 'left'],
-    maxWidth: 490,
-    margin: ['0 auto 40px', null, null, null, '0 0 80px'],
-    h2: {
-      fontSize: [22, null, null, 30, null, 36, 46],
-      lineHeight: [1.6, null, null, 1.41],
-      fontWeight: [500, null, null, 400],
-    },
-    p: {
-      fontSize: ['15px', null, null, '17px'],
-      mt: [3, null, null, 5],
-    },
-  },
-  featureWrapper: {
-    gap: ['40px 20px', null, null, null, '30px'],
-    display: 'grid',
-    gridTemplateColumns: [
-      'repeat(2,1fr)',
-      null,
-      null,
-      'repeat(3,180px)',
-      'repeat(3,1fr)',
-    ],
-    justifyContent: ['unset', null, null, 'center', 'flex-start'],
-  },
-  rightContent: {
-    position: 'relative',
-    mt: [6, null, null, null, 0],
-    maxWidth: '100%',
-    '.swiper-pagination-bullets': {
-      bottom: 20,
-    },
-    '.swiper-pagination-bullet': {
-      backgroundColor: 'rgba(255,255,255,0.5)',
-      width: 10,
-      height: 10,
-      opacity: 1,
-      mx: 5,
-      ':focus': {
-        outline: 0,
-      },
-    },
-    '.swiper-pagination-bullet-active': {
-      backgroundColor: 'rgba(255,255,255,1)',
-    },
-  },
-  progressbar: {
-    position: 'absolute',
-    left: [15, 25],
-    top: [46, 53],
-    zIndex: 2,
-  },
-  progressBar: {
-    position: 'relative',
-    mr: 4,
-  },
-  toggleButton: {
-    backgroundColor: 'transparent',
-    border: 0,
-    cursor: 'pointer',
-    display: 'flex',
-    padding: 0,
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-    '&:focus': {
-      outline: '0 none',
-    },
-  },
-  image: {
+	section: {
+		pt: [10, null, null, null, 60],
+		pb: [30, null, null, 150, 60],
+	},
+	contentWrapper: {
+		gap: [null, null, null, null, '30px'],
+		display: ['flex', null, null, null, 'grid'],
+		alignItems: 'center',
+		flexDirection: ['column', null, null, null, null],
+		gridTemplateColumns: ['unset', null, null, null, 'repeat(1,2fr)'],
+	},
+	leftContent: {
+		m: [0, '30px 0px 0', '30px 0px 0', 0],
+	},
+	heading: {
+		textAlign: ['center', null, null, null, 'left'],
+		maxWidth: 490,
+		margin: ['0 auto 40px', null, null, null, '0 0 80px'],
+		h2: {
 
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'flex-start',
-    '> img': {
-      borderRadius: 10,
-    },
-    figcaption: {
-      backgroundColor: 'primary',
-      display: 'flex',
-      alignItems: 'center',
-      position: 'absolute',
-      left: 0,
-      top: 30,
-      borderRadius: 5,
-      color: '#fff',
-      padding: ['20px 20px 20px 80px', '25px 25px 25px 90px'],
-      h4: {
-        lineHeight: 1,
-        fontWeight: 'bold',
-        fontSize: [14, 18],
-        letterSpacing: 'heading',
-      },
-      p: {
-        fontSize: [13, 16],
-        fontWeight: 500,
-        letterSpacing: 'heading',
-        color: rgba('#fff', 0.6),
-        marginTop: [2],
-      },
-    },
-   
-  },
+			fontSize: [22, null, null, 30, null, 36, 46],
+			lineHeight: [1.6, null, null, 1.41],
+			fontWeight: [500, null, null, 400],
+		},
+		p: {
+			fontSize: ['15px', null, null, '17px'],
+			mt: [3, null, null, 5],
+		},
+	},
+	featureWrapper: {
+		gap: ['40px 20px', null, null, null, '30px'],
+		display: 'grid',
+		gridTemplateColumns: [
+			'repeat(2,1fr)',
+			null,
+			null,
+			'repeat(3,180px)',
+			'repeat(3,1fr)',
+		],
+		justifyContent: ['unset', null, null, 'center', 'flex-start'],
+	},
+	rightContent: {
+		position: 'relative',
+		mt: [6, null, null, null, 0],
+		maxWidth: '100%',
+		'.swiper-pagination-bullets': {
+			bottom: 20,
+		},
+		'.swiper-pagination-bullet': {
+			backgroundColor: 'rgba(255,255,255,0.5)',
+			width: 10,
+			height: 10,
+			opacity: 1,
+			mx: 5,
+			':focus': {
+				outline: 0,
+			},
+		},
+		'.swiper-pagination-bullet-active': {
+			backgroundColor: 'rgba(255,255,255,1)',
+		},
+	},
+	progressbar: {
+		position: 'absolute',
+		left: [15, 25],
+		top: [46, 53],
+		zIndex: 2,
+	},
+	progressBar: {
+		position: 'relative',
+		mr: 4,
+	},
+	toggleButton: {
+		backgroundColor: 'transparent',
+		border: 0,
+		cursor: 'pointer',
+		display: 'flex',
+		padding: 0,
+		position: 'absolute',
+		left: '50%',
+		top: '50%',
+		transform: 'translate(-50%, -50%)',
+		'&:focus': {
+			outline: '0 none',
+		},
+	},
+	image: {
+		position: 'relative',
+		display: 'flex',
+		alignItems: 'flex-start',
+		'> img': {
+			borderRadius: 10,
+		},
+		figcaption: {
+			backgroundColor: 'primary',
+			display: 'flex',
+			alignItems: 'center',
+			position: 'absolute',
+			left: 0,
+			top: 30,
+			borderRadius: 5,
+			color: '#fff',
+			padding: ['20px 20px 20px 80px', '25px 25px 25px 90px'],
+			h4: {
+				lineHeight: 1,
+				fontWeight: 'bold',
+				fontSize: [14, 18],
+				letterSpacing: 'heading',
+			},
+			p: {
+				fontSize: [13, 16],
+				fontWeight: 500,
+				letterSpacing: 'heading',
+				color: rgba('#fff', 0.6),
+				marginTop: [2],
+			},
+		},
+	},
 };
